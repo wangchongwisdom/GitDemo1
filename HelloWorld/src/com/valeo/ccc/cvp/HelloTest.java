c9ed189c624a6da49221e4f51414efda10cfb2fc
Chong Wang <wangchongwisdom@gmail.com>
2020-08-15 12:57:45 -0400

    This is the first

---
+++ HelloWorld/src/com/valeo/ccc/cvp/HelloTest.java
@@ -0,0 +1,11 @@
+package com.valeo.ccc.cvp;
+
+public class HelloTest {
+	
+	public static void main(String args[]) 
+	{ 
+		Hello test1 = new Hello();
+		
+		System.out.println("Hello1, World"); 
+	} 	
+}
